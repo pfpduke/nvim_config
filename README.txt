944d0d5839d1a08a9cde73b75aa1a3b866a3904c
Ian <pfp_duke@yahoo.com>
2025-03-26 17:43:57 -0500

    added nvim_setup script

--- README.txt
+++ README.txt
@@ -1,6 +1,2 @@
-git config --global core.autocrlf true   # Keep LF, even on Windows (good for cross-platform)
-
-winget install --id=sharkdp.fd           # fd is very nice for telescope
-
-winget install zig.zig                   # Install zig Compiler
+Run the nvim_setup.ps1
 
---
+++ nvim_setup.ps1
@@ -0,0 +1,31 @@
+# nvim setup
+
+$tools = @{
+    "Neovim"             = "Neovim.Neovim"
+    "Git"                = "Git.Git"
+    "ripgrep"            = "BurntSushi.ripgrep.GNU"
+    "fd"                 = "sharkdp.fd"
+    "Nerd Font"          = "DEVCOM.JetBrainsMonoNerdFont"
+    "Lua"                = "DEVCOM.Lua"
+    "Zig"                = "Zig.Zig"
+    "Windows Terminal"   = "Microsoft.WindowsTerminal"
+    "PowerShell"         = "Microsoft.PowerShell"
+}
+
+function IsInstalled($packageId) {
+    $installed = winget list --id $packageId 2>$null
+    return $installed -match $packageId
+}
+
+foreach ($tool in $tools.GetEnumerator()) {
+    Write-Host "`nChecking: $($tool.Key)"
+    if (IsInstalled $tool.Value) {
+        Write-Host "$($tool.Key) is already installed"
+    } else {
+        Write-Host "$($tool.Key) not found. Installing..."
+        winget install --id $tool.Value --accept-package-agreements --accept-source-agreements
+    }
+}
+
+Write-Host "`nAll done!"
+Write-Host "Set your terminal font to 'JetBrainsMonoNerdFont' for proper icon rendering."
